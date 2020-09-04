8a50f0e8321ea862fd559de7fc99cd68d5d6e710
vivolscute <55041874+vivolscute@users.noreply.github.com>
2020-09-04 16:30:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ faces= face_cascade.detectMultiScalel(gray, scaleFactor=1.01,minNeighbors=5)
 
 for x, y, w, h in faces:
 
-	img = cv2. rectangle(img, (x, y), (x + w, y+ h), (0, 255, 0),3)
+	img = cv2.rectangle(img, (x, y), (x + w, y+ h), (0, 255, 0),3)
 
 cv2.imshow("cat_face",img)
 cv2.waitkey()
